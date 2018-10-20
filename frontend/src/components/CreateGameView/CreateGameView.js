81f4a56278cf2169779fe4ddac8602dcd838bc17
cjcmattsson <cjcmattsson@gmail.com>
2018-10-21 00:28:42 +0200

    Started working on connection to wikipedia API

--- frontend/src/components/CreateGameView/CreateGameView.js
+++ frontend/src/components/CreateGameView/CreateGameView.js
@@ -6,6 +6,22 @@ class CreateGameView extends Component {
 
   state = {
     secretPerson : "",
+    wikiApiRequest: [],
+  }
+
+  searchWikiApi = () => {
+    if (this.state.secretPerson) {
+      fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=intitle:${this.state.secretPerson}&format=json&srlimit=3&origin=*`)
+      .then(res => res.json())
+      .then((result) => {
+        const searchResults = result.query.search;
+        this.setState({wikiApiRequest: searchResults})
+        console.log(this.state.wikiApiRequest);
+      })
+    }
+    if (this.state.wikiApiRequest === []) {
+      this.setState({wikiApiRequest: false})
+    }
   }
 
   handleChange = (e) => {
@@ -31,9 +47,16 @@ class CreateGameView extends Component {
       <div className="createGameView">
         <h1>Create Game!! :D</h1>
         <form>
-          <input value={this.state.secretPerson}  onChange={this.handleChange} name="secretPerson" type="text" className="person"/>
+          <input value={this.state.secretPerson} onChange={this.handleChange} onKeyUp={this.searchWikiApi} name="secretPerson" type="text" className="person"/>
           <button type="submit" onClick={this.createGame}>Start game</button>
         </form>
+        <ul>
+          {this.state.wikiApiRequest &&
+            this.state.wikiApiRequest.map((person, key) => {
+              return <li key={key}>{person.title}</li>
+            })
+          }
+        </ul>
       </div>
     )
   }
